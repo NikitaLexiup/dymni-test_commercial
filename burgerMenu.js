bac84bedb6eaefb238b6fa2cfad3e24df8febc82
NikitaLexiup <106706979+NikitaLexiup@users.noreply.github.com>
2022-06-21 02:42:18 +0300

    Add files via upload

---
+++ burgerMenu.js
@@ -0,0 +1,35 @@
+"use strict"
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
+
+const menuLinks = document.querySelectorAll('.menu__link');
+if(menuLinks.lenght > 0) {
+    menuLink.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
+
