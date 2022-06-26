c358fbb951b67e214465f10286a32ad9fed2dab5
Nikita Ivanov <106706979+NikitaLexiup@users.noreply.github.com>
2022-06-26 23:29:14 +0300

    Add files via upload

--- burgerMenu.js
+++ burgerMenu.js
@@ -26,27 +26,5 @@ for (let anchor of anchors) {
 }
 
 
-/** const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.lenght > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-
-    function onMenuLinkClick (e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-**/
-
 
 
